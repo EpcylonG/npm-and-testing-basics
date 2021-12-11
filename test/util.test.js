74a1d35b0e2323c9d0d44a58728f385d21f58483
Raul Lopez <raul.lopez@singularfactory.net>
2021-12-11 13:42:59 +0000

    exercises finished

--- test/util.test.js
+++ test/util.test.js
@@ -3,19 +3,17 @@ const util = require("../src/util");
 // Util tests
 
 test("creates an array of 6 elements lol", () => {
-  // TODO implement
-  // expect(util.createArray(6).length).toBe(6);
-
-  // just to let it go red :) cool! now your turn remove this line an implement it you have the clue over this.
-  expect(1).toBe(2);
-});
-
-test("gets the index of a given value", () => {
-  var arrayColors = ["red", "blue", "green"];
-  // TODO implement
+  expect(util.createArray(6).length).toBe(6);
 });
 
 test("moves a string to camel case", () => {
+  var arrayColors = ["red", "blue", "green"];
+  for(let color of arrayColors){
+    expect(util.toCamelCase(color)).toBe(color);
+  }
+});
+
+test("gets the index of a given value", () => {
   var string = "User name";
-  // TODO implement
+  expect(util.getIndexOf(string, 0)).toBe(string.indexOf(0));
 });
